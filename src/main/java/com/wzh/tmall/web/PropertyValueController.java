b54372b44add612ca52dc20d8402d8e61ed8c038
wuzhihang <1397964016@qq.com>
2021-05-22 23:58:01 +0800

    Master

---
+++ src/main/java/com/wzh/tmall/web/PropertyValueController.java
@@ -0,0 +1,50 @@
+/**
+* 模仿天猫整站 springboot 教程 为 wzh 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
+
+package com.wzh.tmall.web;
+import java.util.List;
+
+import com.wzh.tmall.pojo.Product;
+import com.wzh.tmall.pojo.PropertyValue;
+import com.wzh.tmall.service.ProductService;
+import com.wzh.tmall.service.PropertyValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PropertyValueController {
+	@Autowired
+    PropertyValueService propertyValueService;
+	@Autowired
+    ProductService productService;
+
+    @GetMapping("/products/{pid}/propertyValues")
+    public List<PropertyValue> list(@PathVariable("pid") int pid) throws Exception {
+    	Product product = productService.get(pid);
+    	propertyValueService.init(product);
+    	List<PropertyValue> propertyValues = propertyValueService.list(product);
+    	return propertyValues;
+    }
+        
+    @PutMapping("/propertyValues")
+    public Object update(@RequestBody PropertyValue bean) throws Exception {
+    	propertyValueService.update(bean);
+        return bean;
+    }
+    
+  
+}
+
+
+/**
+* 模仿天猫整站 springboot 教程 为 wzh 版权所有
+* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+*/	
